bdad1e76c3e0a325dc47920425f53ff1110759b3
Fırat Oltulu <firat.oltulu@outlook.com>
2022-10-12 22:57:33 +0300

    review and remove ProviderTypes

---
+++ src/SanalPosTR/Configuration/BankTypes.cs
@@ -0,0 +1,19 @@
+﻿namespace SanalPosTR
+{
+    public enum BankTypes
+    {
+        Ziraat = 0,
+        Garanti = 1,
+        Ykb = 2,
+        Akbank = 3,
+        Isbank = 4,
+        DenizBank = 5,
+        HalkBank = 6,
+        VakifBank = 7,
+        FinansBank = 8,
+        Anadolubank = 9,
+        TEB = 10,
+        KuveytTurk = 11,
+        None = 99
+    }
+}
